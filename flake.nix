8a7a957b3be7c30ae7a61b3259ea31fd9e1a1667
EloToJaa <elotoja@protonmail.com>
2025-08-25 18:37:05 +0200

    Allow unfree

--- flake.nix
+++ flake.nix
@@ -4,7 +4,10 @@
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
   outputs = {nixpkgs, ...}: let
-    pkgs = import nixpkgs {system = "x86_64-linux";};
+    pkgs = import nixpkgs {
+      system = "x86_64-linux";
+      config.allowUnfree = true;
+    };
   in {
     devShells.x86_64-linux.default = pkgs.mkShell {
       packages = with pkgs; [
