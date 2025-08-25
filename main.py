1c78a02338c546c753556f86032ec9092d484cea
EloToJaa <elotoja@protonmail.com>
2025-08-25 21:10:34 +0200

    Update state

--- main.py
+++ main.py
@@ -4,9 +4,23 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.remote.webelement import WebElement
 import time
+import json
+
+STATE_PATH = "state.json"
 
 
-def process_li(li: WebElement):
+def read_state():
+    with open(STATE_PATH, "r", encoding="utf-8") as file:
+        data = json.load(file)
+    return data
+
+
+def write_state(data):
+    with open(STATE_PATH, "w", encoding="utf-8") as file:
+        json.dump(data, file, ensure_ascii=False, indent=2)
+
+
+def process_li(li: WebElement, n: int):
     try:
         # li_text = li.text.strip()
         # print(li_text)
@@ -17,33 +31,49 @@ def process_li(li: WebElement):
         )
         # img_src = ul_info.find_element(By.TAG_NAME, "img").get_attribute("src")
         # print(ul_info.text)
-        file.write(ul_info.text.strip() + "\n")
+        with open(f"output-{n}.txt", "a", encoding="utf-8") as file:
+            file.write(ul_info.text.strip() + "\n")
         # print(img_src)
         close_btn = driver.find_element(
             By.XPATH, "/html/body/div[7]/div/div[1]/footer/button"
         )
         close_btn.click()
         time.sleep(2)
+        return True
     except:
-        time.sleep(10)
-        process_li(li)
+        return False
+        # if n >= 10:
+        #     return
+        # time.sleep(10)
+        # print(f"Reprocessing {n}")
+        # process_li(li, n + 1)
 
 
 service = Service(ChromeDriverManager().install())
 driver = webdriver.Chrome(service=service)
 
-driver.get("https://eng.iotexpo.com.cn/sz/ExhibitorList.html?hallNo=9")
+state = read_state()
 
-time.sleep(10)  # Adjust the sleep time as necessary
+for i in range(9, 13):
+    driver.get(f"https://eng.iotexpo.com.cn/sz/ExhibitorList.html?hallNo={i}")
 
-ul_element = driver.find_element(By.ID, "co-main-ct")
-li_elements = ul_element.find_elements(By.TAG_NAME, "li")
+    time.sleep(10)
 
+    ul_element = driver.find_element(By.ID, "co-main-ct")
+    li_elements = ul_element.find_elements(By.TAG_NAME, "li")
 
-with open("output.txt", "a", encoding="utf-8") as file:
+    state[str(i)]["max_counter"] = len(li_elements)
+
     for li in li_elements:
-        process_li(li)
+        title = li.text.strip()
+        if title in state[str(i)]["list"]:
+            continue
 
+        processed = process_li(li, i)
+        if processed:
+            state[str(i)]["list"].append(title)
+            write_state(state)
 
-time.sleep(30)
+    time.sleep(20)
+
 driver.quit()
